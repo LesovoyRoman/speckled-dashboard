546cf2d82ece2f161fa70193b402349bb422d20e
LesovoyRoman <romadesire97@gmail.com>
2019-02-14 16:18:14 +0200

    react-bootstrap sidebar -> (in progress)

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Container  from 'react-bootstrap/Container';
+// import Row  from 'react-bootstrap/Row';
+// import Col  from 'react-bootstrap/Col';
 
+/**
+ * Partials
+ */
+import Sidebar from './../components/partials/Sidebar'
+
+/**
+ * Pages
+ */
 import Dashboard from './../components/pages/Dashboard'
 
 class RouterApp extends Component {
     render() {
         return (
-            <Router>
-                <Route exact path='/' component={ Dashboard } />
-            </Router>
+            <>
+                <Sidebar />
+
+                <Router>
+                    <Route exact path='/' component={ Dashboard } />
+                </Router>
+            </>
         )
     }
 }
