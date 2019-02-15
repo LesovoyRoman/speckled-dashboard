dc8cf2f68a3c505b36ad058e14e395ab947bded2
LesovoyRoman <romadesire97@gmail.com>
2019-02-15 16:08:20 +0200

    sidebar -> App header +

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store'
 import RouterApp from './router'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/modules.scss'
+
+/**
+ * Partials
+ */
+import Sidebar from './components/partials/Sidebar'
+import Header from './components/partials/Header'
 
 class App extends Component {
   render() {
     return (
         <Provider store={ store }>
+            <Sidebar />
+
+            <Header />
+
             <RouterApp />
         </Provider>
     );
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Container  from 'react-bootstrap/Container';
-// import Row  from 'react-bootstrap/Row';
-// import Col  from 'react-bootstrap/Col';
-
-/**
- * Partials
- */
-import Sidebar from './../components/partials/Sidebar'
 
 /**
  * Pages
@@ -18,8 +10,6 @@ class RouterApp extends Component {
     render() {
         return (
             <>
-                <Sidebar />
-
                 <Router>
                     <Route exact path='/' component={ Dashboard } />
                 </Router>
